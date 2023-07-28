71626c2df4ce30deca854a11fcfb1bc7d681a7c0
nicolas ♺ <125375345+odevnicolas@users.noreply.github.com>
2023-07-28 18:27:49 -0300

    atualização

--- src/components/Social/social.tsx
+++ src/components/Social/social.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import { FaWhatsapp, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import 'tailwindcss/tailwind.css';
@@ -5,10 +6,10 @@ import 'tailwindcss/tailwind.css';
 const RedesSociais: React.FC = () => {
   return (
     <div className="z-20 rounded-s-[45px] bg-verde-social p-6 w-34 mt-[19%] float-right fixed top-0 right-0">
-      <a href="https://api.whatsapp.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://wa.me/558173265915?text=Ol%C3%A1%20eu%20desejo%20falar%20com%20a%20Beemed." target="_blank" rel="noopener noreferrer">
         <FaWhatsapp size={32} className="z-20 mb-5 text-verde-social-icons hover:text-verde-social-icons-hover" />
       </a>
-      <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.instagram.com" target= "_blank" rel="noopener noreferrer">
         <FaInstagram size={32} className="z-20 mb-5 text-verde-social-icons hover:text-verde-social-icons-hover" />
       </a>
       <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
