38b6f072b9cdbc047840f490bef6472b348f2aff
Nicolas Sousa <nicolas@mentat.solutions>
2023-07-29 01:25:33 -0300

    fix: medic screen

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -13,17 +13,17 @@ const Sobre: React.FC = () => {
       <Social />
       <div className="-z-20 relative">
         <img src="./beemed.png" className=" -z-30 -mt-[8%]" />
-        <div className="relative -mt-[50%] py-8 z-0">
+        <div className="relative 2xl:-mt-[50%] py-8 xl:-mt-[50%] lg:-mt-[50%] md:-mt-[50%] sm:-mt-[50%] xsm:-mt-[50%] z-0">
           <img src="./sobre.png" width={515} height={660} />
-        </div>
-        <div className=" flex justify-center items-center -mt-[47%] z-20">
+        <div className="flex justify-center items-center 2xl:-mt-[38%] 2xl:z-99 xl:z-99 xl:-mt-[58%] lg:-mt-[75%] lg:z-20 md:-mt-[80%] sm:-mt-[85%] xsm:-mt-[90%] md:z-20">
           <img src="./tablet.svg" width={1000} height={280} />
+        </div>
         </div>
       </div>
 
-      <div className="ml-[13%] mt-[18%] bg-[#9BF798]  rounded-3xl w-[71%] h-[1%] z-10">
+      <div className="ml-[13%] 2xl:mt-[18%] xl:mt-[18%] lg:mt-[18%] md:mt-[25%] sm:mt-[35%] xsm:mt-[45%] bg-[#9BF798]  rounded-3xl w-[71%] h-[1%] z-10">
         <div className=" px-[7%] py-[4%]">
-          <p className="text-[16sp] leading-8 font-normal">
+          <p className="text-[16px] leading-8 font-normal xl:text-sm">
             A Beemed sonha em tornar as cirurgias e os
             <br /> procedimentos médicos acessíveis para mais pessoas. Para{' '}
             <br /> alcançar esse objetivo, a empresa negocia <br /> previamente
@@ -35,7 +35,7 @@ const Sobre: React.FC = () => {
           <button className="text-black bg-[#FAFAFA] rounded-xl py-[0.8%] px-[0.8%] hover:bg-slate-300 mt-[2.5%] font-medium">
             Fale com a gente !
           </button>
-          <div className=" float-right -mt-[29%] mr-[3%] ">
+          <div className=" float-right -mt-[29%] mr-[3%] xl:mr-0 lg:invisible md:invisible sm:invisible xsm:invisible">
             <img
               src="./sobre-container.svg"
               alt=""
