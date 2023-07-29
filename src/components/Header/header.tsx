269dfaeafaca3e53ddf248aa78320c830fd011e4
nicolas ♺ <125375345+odevnicolas@users.noreply.github.com>
2023-07-29 00:49:19 -0300

    atualização

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,47 +1,80 @@
 /* eslint-disable prettier/prettier */
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
-import Link from 'next/link'
+import Link from 'next/link';
+import Modal from '@/components/AuthModal/modal';
 import 'tailwindcss/tailwind.css';
-import { useRouter } from 'next/router';
+import { Fragment } from 'react';
 
-const HeaderProced = () => {
-  const router = useRouter();
+interface HeaderProps {
+  activeRoute: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ activeRoute }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
-    <header className="flex flex-col sm:flex-row justify-between items-center py-[1.3%] px-[2%] mt-[1.3%]">
-      <div className="ml-[2%] z-10">
-        <Link href="/">
-          <img src="/BeemedBlack.svg" alt="Logo da Empresa" width={140} height={140} />
-        </Link>
-      </div>
-      <div className="flex gap-10 z-10">
-        <Link className={`mt-[2%] ${router.pathname === '/cirurgias' ? 'underline text-green-500' : 'hover:underline'}`} href="/cirurgias">
-          <button className={`text-black font-medium ${router.pathname === '/cirurgias' ? 'text-green-500' : 'hover:underline decoration-solid decoration-colorButton decoration-2 box-decoration-clone'}`}>Cirurgias</button>
-        </Link>
-        <Link className={`mt-[2%] ${router.pathname === '/estetica' ? 'underline text-green-500' : ''}`} href="/estetica">
-          <button className={`text-black font-medium ${router.pathname === '/estetica' ? 'text-green-500' : 'hover:underline decoration-solid decoration-colorButton decoration-2 box-decoration-clone'}`}>Estética</button>
-        </Link>
-        <Link className={`mt-[2%] ${router.pathname === '/exames' ? 'underline text-green-500' : ''}`} href="/exames"> 
-          <button className={`text-black font-medium ${router.pathname === '/exames' ? 'text-green-500' : 'hover:underline decoration-solid decoration-colorButton decoration-2 box-decoration-clone'}`}>Exames</button>
-        </Link>  
-        <Link className={`mt-[2%] ${router.pathname === '/odontologia' ? 'underline text-green-500' : ''}`} href="/odontologia"> 
-          <button className={`text-black font-medium ${router.pathname === '/odontologia' ? 'text-green-500' : 'hover:underline decoration-solid decoration-colorButton decoration-2 box-decoration-clone'}`}>Odontologia</button>
-        </Link>  
-        <Link className={`mt-[2%] ${router.pathname === '/telemedicina' ? 'underline text-green-500' : ''}`} href="/telemedicina"> 
-          <button className={`text-black font-medium ${router.pathname === '/telemedicina' ? 'text-green-500' : 'hover:underline decoration-solid decoration-colorButton decoration-2 box-decoration-clone'}` }>Telemedicina</button>
-        </Link>  
-      </div>
-      <div>
-      <Link className='mt-[1.2%]'href="/">
-        <button className="text-black btn-procedimentos ml-5 font-NowAltRegular">Tela inicial</button>
-      </Link>  
-      </div>
-    </header>
+    <Fragment>
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal} />
+      <header className="flex justify-between items-center py-[1.3%] px-[2%] mt-[1.3%] sm:px-0">
+        <div className="ml-[2%] z-10">
+          <Link href="/">
+            <img
+              src="/BeemedBlack.svg"
+              alt="Logo da Empresa"
+              width={140}
+              height={140}
+            />
+          </Link>
+        </div>
+        <div className="ml-[9%] flex gap-10 z-10">
+          <Link className="mt-[2%]" href="/sobre">
+            <button
+              className={`text-black font-medium hover:underline decoration-solid decoration-colorButton decoration-2 box-decoration-clone ${
+                activeRoute === '/sobre' ? 'text-green-500' : 'text-black'
+              }`}
+            >
+              Sobre
+            </button>
+          </Link>
+          <Link className="mt-[2%]" href="/medico">
+            <button
+              className={`text-black font-medium hover:underline decoration-solid decoration-colorButton decoration-2 box-decoration-clone ${
+                activeRoute === '/medico' ? 'text-green-500' : 'text-black'
+              }`}
+            >
+              Cadastro Médico
+            </button>
+          </Link>
+          <Link className="mt-[2%]" href="/clinico">
+            <button className="text-black font-medium hover:underline decoration-solid decoration-colorButton decoration-2 box-decoration-clone">
+              Cadastro Clínicas/Hospitais
+            </button>
+          </Link>
+          <button
+            className="text-black font-medium mt-[1%] hover:underline decoration-solid decoration-colorButton decoration-2 box-decoration-clone"
+            onClick={handleOpenModal}
+          >
+            Login
+          </button>
+          <Link className="mt-[1.2%]" href="/cirurgias">
+            <button className="text-black btn-procedimentos ml-5 font-NowAltRegular font-NowAlt-Black">
+              Procedimentos
+            </button>
+          </Link>
+        </div>
+        <div></div>
+      </header>
+    </Fragment>
   );
 };
 
-export default HeaderProced;
-
+export default Header;
